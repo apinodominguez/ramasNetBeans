e140f0f7909aaf4c88c7c2440d6aa147665ba26b
apinodominguez <apinodominguez@nautilus3.danielcastelao.org>
2018-11-08 09:16:14 +0100

    Ejercicio ramas. Commit F. Rama principal

--- ramasNetBeans/src/ramasnetbeans/RamasNetBeans.java
+++ ramasNetBeans/src/ramasnetbeans/RamasNetBeans.java
@@ -4,7 +4,7 @@ public class RamasNetBeans {
 
     public static void main(String[] args) {
        // Comentario para añadir el punto B a la rama principal
-       // Comentario para añadir el punto C a la rama principal
+       // Comentario para añadir el punto F a la rama principal
     }
     
 }
